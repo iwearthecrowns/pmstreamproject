0cd5d263e46d491e6fd790584b9b9492661196f4
iwearthecrowns <iwearthecrowns@users.noreply.github.com>
2020-04-02 15:33:18 -0400

    Added second sprite & say blocks

--- main.ts
+++ main.ts
@@ -17,4 +17,25 @@ let mySprite = sprites.create(img`
 . . . . . . . 8 8 f 8 8 . . . . . . . 
 `, SpriteKind.Player)
 scene.setBackgroundColor(11)
-mySprite.say("eh")
+mySprite.say("eh", 2000)
+let mySprite2 = sprites.create(img`
+. . . . . . . . . . . . . . . . . . . 
+. . . . 8 8 8 8 8 8 8 8 8 8 8 . . . . 
+. . . . 8 . 4 4 4 4 4 4 4 . 8 . . . . 
+. . . 8 . 4 4 e e e e e 4 4 . 8 . . . 
+. . 8 . e e e e e e e e e e e . 8 . . 
+. . 8 e e e e e e e e e e e e e 8 . . 
+. 8 8 e e e e e e e e e e e e e 8 8 . 
+. 8 8 e e e e e e e e e e e e e 8 8 . 
+. 8 8 e e e e f e e e f e e e e 8 8 . 
+. . 8 e e e e e e e e e e e e e 8 . . 
+. . . e e e e e e e e e e e e e . . . 
+. . . e e f e e e e e e e f e e . . . 
+. . . . e e f e e e e e f e e . . . . 
+. . . . . e e f f f f f e e . . . . . 
+. . . . . . e e e e e e e . . . . . . 
+. . . . . . . 2 2 f 2 2 . . . . . . . 
+`, SpriteKind.Player)
+mySprite2.setPosition(100, 60)
+pause(2000)
+mySprite2.say("hello")
